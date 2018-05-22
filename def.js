afddd98f28069e4bdad0379ca056df867b716ce2
Gino Wang <sihengwang2019@u.northwestern.edu>
2018-05-22 14:49:43 -0500

    Refined narrative API definition

---
+++ def.js
@@ -0,0 +1,105 @@
+let stage = {
+	// stage number 
+	stage_number : 2
+	// chapters in this stage
+	chapters : [] 
+	// progression of the stages
+	prev_stage : 1
+	next_stage : 3
+	// progression rule of the chapters
+	chapter_map : { B : D; 
+						 B : E;
+						 C : E;
+						 // example ... other chapter relationships 
+						}
+}
+
+var chapter = { 
+	// list of stage it belongs to 
+	stages : []
+	// context of the chapter 
+	setting : setting
+	// list of characters it contains
+	characters : []
+	// list of objects it contains
+	objects : []
+
+	// chapter number 
+	chaper_chapter : 2
+	// next chapter as determined by character actions taken 
+	next_chapter : 2
+
+	// optional 
+	transition : transition
+
+	// static affordance for participants
+	// find all participants for chapter
+	find_participants_for_chapter : function(context) {}
+	// find participants for each character
+	find_participants_for_character : function(character.context.chapter_number) {}
+
+	// at the end of chapter, do things based on narrative-specific character attributes
+	charater_modifier : function(character[]) {}
+	// at the end of chapter, do things based on narrative-specific object attributes
+	object_modifier : function (object[]) {}
+}
+
+var character = {
+	// list of chapters this character appears
+	owner_chapters : []
+	// list of objects owned by this character (at any time in narrative)
+	owned_objects  : []
+
+	// context for a character is different in different chapters
+	context : { 
+					chapter_number : { 
+											  detector : detector
+										  }
+					// ... and more of this struct
+				 }
+				 
+	// action for a character is different in different chapters
+	// an action can change next chapter, character representation and object representation
+	action :  { 
+					chapter_number : { 
+												action_description  : "" 
+												change_next_chapter : function(chapter_number, next_chapter_number){} 
+												change_character    : function(character[]) {}
+												change_object       : function(object[]) {}
+											 }
+					// ... and more of this struct
+			 	 }
+
+	// current participant, not sure if needed
+	current_participant : participant
+}
+
+var object = { 
+	// name of object, like a super potion
+	name : ""
+	// list of owners, mutable 
+	owner_characters : []
+}
+
+var setting = {
+	context
+}
+
+let context = {
+	location
+	time
+	weather
+	day
+	// ... other stuff
+}
+
+let transition = {
+	end_chapter
+	end_chapter.character
+	begin_chapter.character 
+	object
+}
+
+let participant = {
+	context
+}
